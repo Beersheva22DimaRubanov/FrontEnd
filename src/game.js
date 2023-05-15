1d25e8bdffd3c8122e30cc4b010c198c2076ac5e
Rubanov Dmitri <rubanovdp@gmail.com>
2023-05-15 15:08:36 +0300

    add another game

--- src/game.js
+++ src/game.js
@@ -1,77 +1,103 @@
 //configuration
-const nMoves = 3;
+let nMoves;
 //elements
 const inputElement = document.getElementById("input-id");
 const goButtonElement = document.getElementById("go-id");
-const squareElements = document.getElementsByClassName("square");
+const inputGroup = document.getElementsByClassName("input-group")[0];
+let squareElements = [];
+const squareSection = document.getElementById('square-group');
 const resultMessageElement = document.getElementById("game-result");
 const playAgainButtonElement = document.getElementById("play-again-id");
+const readyButton = document.getElementById("ready-button");
+const openLetterButton = document.getElementById("letter-button");
+const inpuNumber = document.getElementById("input-number");
+const startButton = document.getElementById("start-button")
 
 //global variables
-let count = 0;
-const word = 'align';
+let count;
+let openLetters = 0;
+let word = '';
+const words = [ ['OOP programming language', 'java'], ['The highest mountain in the world?', 'everest'] ];
 isWinner = false;
 
 //functions
 
 function game() {
+    
     countGreen = 0;
     const inputWord = inputElement.value;
-    for (let i = 0; i < word.length; i++) {
-        let char = inputWord.charAt(i);
-        if (checkChar(char, squareElements[i], i) == 'green') {
-            countGreen++;
-        }
-    }
-    count++;
+    Array.from(squareElements).forEach(elem =>{
+        elem.style.background = 'white'
+    })
+    
     inputElement.value = "";
-    if (count == nMoves || countGreen == word.length) {
-        if(countGreen == word.length){
+    
+    
+        if(inputWord == word){
             isWinner = true;
         }
         finishGame();
-    }
-
 }
 
-function checkChar(char, squareElement, index) {
-    let color = 'red';
-    let count = 1;
-    for (i = 0; i < word.length; i++) {
-        if (word.charAt(i) == char && i == index) {
-            color = 'green';
-            count++;
-        } else if (word.charAt(i) == char && i != index) {
-            color = 'yellow';
-        }
+function showLetter(){
+    const num = inpuNumber.value;
+    if(num < 1 || num > word.length){
+        alert("Wrong number")
+    } else{
+        squareElements[num-1].style.backgroundColor = 'white';
+        openLetters++;
+        inpuNumber.value = '';
     }
-    squareElement.innerHTML = char;
-    squareElement.style.backgroundColor = color;
-    return color;
+    if(openLetters >= count){
+        openLetterButton.disabled = true;
+        goButtonElement.disabled = false;
+        readyButton.style.display = 'none';
+    } 
+}
+
+function setWord(){
+    let index = Math.floor(Math.random() * (words.length ));
+    document.getElementById('question').innerHTML = words[index][0];
+    word = words[index][1];
+    count = Math.floor(word.length/2);
+    nMoves = count + 1;
+    const wordArray = Array.from(word);
+    const divsArray = wordArray.map(letter => `<div class ="square"> ${letter} </div>`)
+    squareSection.innerHTML = divsArray.join('');
+    squareElements = document.getElementsByClassName('square');
+
 }
 
 function startGame() {
-    count = 0;
+    setWord();
+    squareSection.style.display = 'flex';
+    inputGroup.style.display = 'flex';
     inputElement.readOnly = false;
     goButtonElement.disabled = false;
-    for (i = 0; i < squareElements.length; i++) {
-        squareElements[i].style.backgroundColor = 'white';
-        squareElements[i].innerHTML = '';
-    }
     isWinner = false;
-    resultMessageElement.style.display = '';
+    resultMessageElement.innerHTML = '';
     playAgainButtonElement.hidden = true;
+    goButtonElement.disabled = true;
+    openLetterButton.disabled = false;
+    readyButton.style.display = 'block';
 }
 
 function finishGame() {
     inputElement.readOnly = true;
     goButtonElement.disabled = true;
-    resultMessageElement.innerHTML = isWinner ? "You are the Winner!" : "Try one more time";
+    resultMessageElement.innerHTML = isWinner ? "You are the Winner!" : "You are wrong. Try one more time";
     playAgainButtonElement.hidden = false;
+}
+
+function showButton(){
+    openLetterButton.disabled = true;
+    goButtonElement.disabled = false;
+    readyButton.style.display = 'none';
 }
 
 //Actions
 
 goButtonElement.addEventListener("click", game);
 playAgainButtonElement.addEventListener("click", startGame);
-startGame();
+openLetterButton.addEventListener("click", showLetter)
+readyButton.addEventListener("click", showButton)
