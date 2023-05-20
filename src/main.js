98b8500b2a6b97b9ae37a2ce36a3eafb0399f9ff
Rubanov Dmitri <rubanovdp@gmail.com>
2023-05-21 00:09:46 +0300

    add movies

--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
+import moviesObj from '../movies.json' assert {type: 'json'};
 const detailsImageElement = document.querySelector(".details-image");
+const thumbNailsList = document.querySelector(".thumbnails-list");
 const detailsTitleElement = document.querySelector(".details-title");
-const thumbNailsAnchor = document.querySelectorAll(".thumbnails-anchor");
 const detailsSection = document.querySelector(".details-section");
 const mainSection = document.querySelector("main")
 const HIDDEN = "hidden";
 const POINT = "point";
+console.log(moviesObj.httpPrefix);
+
+
+
+function getPuictures() {
+    let listItems = moviesObj.results.map((movie) => {
+        const listItem =
+            `<li class = "thumbnails-item">
+            <a href = '#' class = "thumbnails-anchor"
+            data-details-image = "${moviesObj.httpPrefix + movie.backdrop_path}" data-details-text = "${movie.overview.slice(0,100)}">
+            <img src="${moviesObj.httpPrefix + movie.poster_path}" class="thumbnails-image">
+    <span class="thumbnails-title">${movie.original_title}</span>
+    </a> 
+    </li>`
+        return listItem;
+    });
+
+    return listItems.join("");
+}
+
+thumbNailsList.innerHTML = getPuictures();
+const thumbNailsAnchor = document.querySelectorAll(".thumbnails-anchor");
+console.log(thumbNailsAnchor)
 
 thumbNailsAnchor.forEach(anchor => anchor.addEventListener("click", setDetails.bind(undefined, anchor)))
+
 
 function setDetails(anchor) {
     showDetails();
@@ -17,7 +42,7 @@ function setDetails(anchor) {
 function showDetails() {
     mainSection.classList.remove(HIDDEN);
     detailsSection.classList.add(POINT);
-    setTimeout(function(){
+    setTimeout(function () {
         detailsSection.classList.remove(POINT);
     })
 }
@@ -25,4 +50,6 @@ function showDetails() {
 function hideDetails() {
     mainSection.classList.add(HIDDEN);
 }
+
+window.hideDetails = hideDetails;
 
